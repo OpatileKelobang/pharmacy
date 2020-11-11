bd0da604bb7ca333bb78f6fb57e276489305b9d7
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-11 19:53:26 +0200

    Changed nethod to expose IDs

--- src/main/java/digital/epharmacy/pharmacy/config/MyDataRESTConfig.java
+++ src/main/java/digital/epharmacy/pharmacy/config/MyDataRESTConfig.java
@@ -1,23 +1,31 @@
 package digital.epharmacy.pharmacy.config;
 
-import digital.epharmacy.pharmacy.entity.Product;
-import digital.epharmacy.pharmacy.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class MyDataRESTConfig implements RepositoryRestConfigurer {
 
+    private EntityManager entityManager;
 
+    @Autowired
+    public MyDataRESTConfig(EntityManager injectedEntityManager){
+        entityManager = injectedEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
         // Expose ID in JSON
-        config.exposeIdsFor(Product.class);
-        config.exposeIdsFor(ProductCategory.class);
+        exposeIds(config);
 
         /*HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
@@ -32,5 +40,19 @@ public class MyDataRESTConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));*/
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType temEntityType : entities) {
+            entityClasses.add(temEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
     }
 }
