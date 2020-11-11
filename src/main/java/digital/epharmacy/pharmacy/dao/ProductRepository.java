3e8d9d1350c60e998fdbafdfe28f2974db12cbab
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-11 14:21:32 +0200

    CORS security disable

--- src/main/java/digital/epharmacy/pharmacy/dao/ProductRepository.java
+++ src/main/java/digital/epharmacy/pharmacy/dao/ProductRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin("http://localhost:4200")
+
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost", "https://epharmacy-online.herokuapp.com"})
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
