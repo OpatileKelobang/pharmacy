4f2d68e2cc235cad3c3daadec576dcd0f8fb7472
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-11 04:21:28 +0200

    Added find by ID query

---
+++ src/main/java/digital/epharmacy/pharmacy/config/MyDataRESTConfig.java
@@ -0,0 +1,29 @@
+package digital.epharmacy.pharmacy.config;
+
+import digital.epharmacy.pharmacy.entity.Product;
+import digital.epharmacy.pharmacy.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRESTConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable HTTTP methods for Product: PUT, POST amd DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Disable HTTTP methods for ProductCategory: PUT, POST amd DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
