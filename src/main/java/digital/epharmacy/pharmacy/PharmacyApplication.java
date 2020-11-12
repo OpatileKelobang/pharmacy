3fa8f2f44dd88c891b09ab10151ad605ec7f4188
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-12 02:29:01 +0200

    added bean to disable cors

--- src/main/java/digital/epharmacy/pharmacy/PharmacyApplication.java
+++ src/main/java/digital/epharmacy/pharmacy/PharmacyApplication.java
@@ -4,6 +4,8 @@ import digital.epharmacy.pharmacy.config.MyIntercepter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.MappedInterceptor;
 
 @SpringBootApplication
@@ -11,6 +13,17 @@ public class PharmacyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PharmacyApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:3000", "https://epharmacy-online.herokuapp.com", "https://amahlathini.github.io/admin-dashboard", "https://vibekonnect.tech/admin-dashboard");
+            }
+        };
     }
 
     @Bean
