734b0047b158fc327d83bb61d9067e56122ec5a2
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-12 13:35:08 +0200

    Changed collection name

--- src/main/java/digital/epharmacy/pharmacy/dao/ProductCategoryRepository.java
+++ src/main/java/digital/epharmacy/pharmacy/dao/ProductCategoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://localhost", "https://epharmacy-online.herokuapp.com", "https://vibekonnect.tech/admin-dashboard", "https://vibekonnect.tech/admin-dashboard", "https://vibekonnect.tech"})
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "categories", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
