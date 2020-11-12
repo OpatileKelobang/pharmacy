65b1c027b10cae1e47d81c1c2f9f54a0c3a785f0
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-12 03:17:46 +0200

    Refactro CORS config

--- src/main/java/digital/epharmacy/pharmacy/dao/ProductCategoryRepository.java
+++ src/main/java/digital/epharmacy/pharmacy/dao/ProductCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost", "https://epharmacy-online.herokuapp.com", "https://vibekonnect.tech/admin-dashboard/", "https://vibekonnect.tech/admin-dashboard/", "https://vibekonnect.tech"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost", "https://epharmacy-online.herokuapp.com", "https://vibekonnect.tech/admin-dashboard", "https://vibekonnect.tech/admin-dashboard", "https://vibekonnect.tech"})
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
